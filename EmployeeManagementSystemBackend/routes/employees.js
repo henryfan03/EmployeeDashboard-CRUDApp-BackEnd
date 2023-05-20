0d9264e3338b51f3b4ac84c0a6e2f3bbea57bbae
Henry Fan <henryfan@macbook-air.mynetworksettings.com>
2023-05-20 00:54:34 -0400

    fixed spelling mistake

--- EmployeeManagementSystemBackend/routes/employees.js
+++ EmployeeManagementSystemBackend/routes/employees.js
@@ -10,56 +10,56 @@ const ash = require("express-async-handler");
 
 /** GET ALL employees */
 router.get(
-  "/",
-  ash(async (req, res) => {
-    let employees = await Employee.findAll({ include: [Task] });
-    res.status(200).json(employees);
-  })
+    "/",
+    ash(async (req, res) => {
+        let employees = await Employee.findAll({ include: [Task] });
+        res.status(200).json(employees);
+    })
 );
 
 /** GET employee BY ID*/
 router.get(
-  "/:id",
-  ash(async (req, res) => {
-    let employee = await Employee.findByPk(req.params.id, { include: [Task] });
-    res.status(200).json(employee);
-  })
+    "/:id",
+    ash(async (req, res) => {
+        let employee = await Employee.findByPk(req.params.id, { include: [Task] });
+        res.status(200).json(employee);
+    })
 );
 
 // Delete employee
 router.delete(
-  "/:id",
-  ash(async (req, res) => {
-    await Employee.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    res.status(200).json("Employee deleted");
-  })
+    "/:id",
+    ash(async (req, res) => {
+        await Employee.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json("Employee deleted");
+    })
 );
 
-// Add new instructor
+// Add new employee
 router.post(
-  "/",
-  ash(async (req, res) => {
-    let newEmployee = await Employee.create(req.body);
-    res.status(200).json(newEmployee);
-  })
+    "/",
+    ash(async (req, res) => {
+        let newEmployee = await Employee.create(req.body);
+        res.status(200).json(newEmployee);
+    })
 );
 
 // Edit employee
 router.put(
-  "/:id",
-  ash(async (req, res) => {
-    await Employee.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    let employee = await Employee.findByPk(req.params.id, { include: [Task] });
-    res.status(201).json(employee);
-  })
+    "/:id",
+    ash(async (req, res) => {
+        await Employee.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        let employee = await Employee.findByPk(req.params.id, { include: [Task] });
+        res.status(201).json(employee);
+    })
 );
 
 // Export our router, so that it can be imported to construct our apiRouter;
